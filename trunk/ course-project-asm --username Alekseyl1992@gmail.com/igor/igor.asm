1c5f14778609a04d76616c501646313d408f71d7
igorcoding@gmail.com <igorcoding@gmail.com@bd953650-356d-83d3-37a3-a6637666e4e7>
2013-04-15 07:26:03 +0000

    basic tsr bla-bla

--- trunk/ course-project-asm --username Alekseyl1992@gmail.com/igor/igor.asm
+++ trunk/ course-project-asm --username Alekseyl1992@gmail.com/igor/igor.asm
@@ -1,20 +1,28 @@
 ;тестировочный проект (igor)
-
-.model small
-.data
-
-.code
+IgorPart segment PARA
+assume CS:IgorPart, DS:IgorPart
 org 100h
-igorMain:
 
-	call exit
-
-
-	exit proc c uses AX
-		mov AH, 4Ch
-		int 21h
-		int 20h
-		ret
-	exit endp
+;резидентная часть
+_start:
+	jmp _loadTSR
+	old DD 0
 	
-end igorMain
+	_endProgram
+	_loadTSR:
+		mov AH, 35h               
+		mov AL, XXh					; получение адреса старого обработчика
+		int 21h                     ; прерываний от таймера
+		mov WORD ptr old, BX        ; сохранение смещения обработчика
+		mov WORD ptr old + 2, ES    ; сохранение сегмента обработчика
+		mov AH, 25h
+		mov AL, XXh					 ; установка адреса нашего обработчика
+		;mov DX,  offset _proc        ; указание смещения нашего обработчика
+		int 21h                      ; вызов DOS
+		mov AX, 3100h                ; функция DOS завершения резидентной программы
+		mov DX, (_endProgram - _start + 10Fh) / 16 ; определение размера резидентной
+												   ; части программы в параграфах
+		int 21h                  	; вызов DOS
+	
+IgorPart ends					 	 ; конец кодового сегмента
+end _start							 ; конец программы
